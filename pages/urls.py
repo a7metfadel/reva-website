98ec9e64b843965b38e78304ac2bde35fca9a515
a7metfadel <ahmetfad3l@gmail.com>
2025-04-13 13:41:12 +0300

    reva 1

---
+++ pages/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path , include
+from . import views
+
+urlpatterns = [
+    path('home/', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('job/', views.job, name='job'),
+    path('report/', views.report, name='report'),
+    path('product_info/', views.product_info, name='product_info'),
+    path('labaratory/', views.labaratory, name='labaratory'),
+    path('General/', views.General, name='General'),
+    path('cephalosporins/', views.cephalosporins, name='cephalosporins'),
+    path('benicillins/', views.benicillins, name='benicillins'),
+]
